2c729cfc7c799d42d76f3b356408582eb90807c8
DonOh <201904014@o.cnu.ac.kr>
2022-05-15 16:29:59 +0900

    Created by donoh

---
+++ test1/calculator.py
@@ -0,0 +1,25 @@
+def add(x,y):
+    return None
+
+def sub(x,y):
+    return None
+
+def multiply(x,y):
+    return None
+
+def divide(x,y):
+    return None
+
+def main():
+    x,y = 8,2
+    res1 = add(x,y)
+    res2 = sub(x,y)
+    res3 = multiply(x,y)
+    res4 = divide(x,y)
+    print(f'{x} + {y} = {res1}')
+    print(f'{x} - {y} = {res2}')
+    print(f'{x} * {y} = {res3}')
+    print(f'{x} / {y} = {res4}')
+
+if __name__=='__main__':
+    main()
